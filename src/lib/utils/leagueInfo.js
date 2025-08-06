4bae1ea256544d14cac0b8b2f5a51d03bb59bc52
Zuid81 <133366025+Zuid81@users.noreply.github.com>
2025-08-06 14:25:21 +0200

    Update leagueInfo.js
    League ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1048184735417126912"; // your league ID
+export const leagueID = "1180400659823755264"; // your league ID
 export const leagueName = "Guinness Dynasty League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
